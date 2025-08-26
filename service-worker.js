190738ff9c00c0e2990bce8960690c24489c5b49
drmaluendaia-maker <dr.maluenda.ia@gmail.com>
2025-08-26 18:05:17 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Nombre del caché para nuestra aplicación
-const CACHE_NAME = 'notti-calc-v11'; // Incrementamos la versión del caché
+const CACHE_NAME = 'notti-calc-v12'; // Incrementamos la versión del caché
 
 // Lista de archivos esenciales para que la app funcione offline
 const urlsToCache = [
